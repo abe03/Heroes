19bcffdf18cd6ef2ad1a41e9da3d09cf31767785
Abe Coronado <aecc03@gmail.com>
2015-11-18 03:05:29 -0800

    Add slider images and slider functionality
    *Add Css styling for pagenation
    *Add JS to init responsive slides

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	//slider init
+	$("#slides").responsiveSlides({
+		speed: 700, //speed of transition
+		timeout: 3500, //time between slide transitions
+		pager: false //show the pagination
+	});
+});
